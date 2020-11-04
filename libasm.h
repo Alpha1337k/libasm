ae12c9bb199fa807a438d80f4eec2304a4b4205a
Oscar Kruithof <alpha_1337@Oscars-MacBook-Pro.local>
2020-11-04 21:43:38 +0100

    updated to turned in files

--- libasm.h
+++ libasm.h
@@ -6,7 +6,7 @@
 /*   By: alpha <alpha@student.codam.nl>               +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2020/10/04 17:52:12 by alpha         #+#    #+#                 */
-/*   Updated: 2020/10/05 17:01:05 by okruitho      ########   odam.nl         */
+/*   Updated: 2020/10/06 14:28:46 by okruitho      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <sys/errno.h>
 int		ft_strlen(char *s);
-int 		ft_strcmp(char *s1, char *s2);
+int 	ft_strcmp(char *s1, char *s2);
 char 	*ft_strcpy(char *dest, const char *src);
 int     ft_write(int fd, char *buf, int len);
 int     ft_read(int fd, char *buf, int len);
